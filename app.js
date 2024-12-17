cd09b579596dd854f32200dfbda18d8892f89548
shila <shilapattewar16@gmail.com>
2024-12-17 14:06:40 -0600

    added for loop to get inventory list

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const adventurer = {
     name: "Robin",
     health: 10,
-    inventory: ["sword", "potion", "artifact"]
+    inventory: ["sword", "potion", "artifact"],
+    companion: {
+        name: "Leo",
+        type: "Cat"
+    }
     }
 
     console.log(adventurer.inventory[0]);
+
+
+    // create a loop that logs each item in Robin’s inventory.
+    for ( i=0 ; i< adventurer.inventory.length ; i++)
+        {
+            console.log (adventurer.inventory[i]);
+        }
